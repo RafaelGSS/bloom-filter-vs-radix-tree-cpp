852990ce4a7885eadbc7b7b8dad592bf587456bc
RafaelGSS <rafael.nunu@hotmail.com>
2022-08-05 13:59:57 -0300

    fix: swap list

--- src/radix_tree.h
+++ src/radix_tree.h
@@ -21,8 +21,30 @@ public:
 
   virtual Node* CreateChild(std::string prefix) {
     char label = prefix[0];
-    children[label] = new Node(prefix);
-    return children[label];
+
+    Node* child = children[label];
+    if (child == nullptr) {
+      children[label] = new Node(prefix);
+      return children[label];
+    }
+
+    int i = 0;
+    int prefix_len = prefix.length();
+    for (; i < child->prefix.length(); ++i) {
+      if (i > prefix_len || prefix[i] != child->prefix[i]) {
+        std::string parent_prefix = child->prefix.substr(0, i);
+        std::string child_prefix = child->prefix.substr(i);
+
+        child->prefix = child_prefix;
+        Node* split_child = new Node(parent_prefix);
+        split_child->children[child_prefix[0]] = child;
+        children[parent_prefix[0]] = split_child;
+
+        // create prefix child
+        return split_child->CreateChild(prefix.substr(i));
+      }
+    }
+    return child->CreateChild(prefix.substr(i));
   }
 
   virtual Node* CreateWildcardChild() {
